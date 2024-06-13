63e496772d8748210f3b61e7aef58a9fac68ef1b
admin-at-595115466597 <willhwli@amazon.com>
2024-06-13 20:48:05 +0800

    modify agent

--- listing_voc_agents.py
+++ listing_voc_agents.py
@@ -49,113 +49,6 @@ bedrock_llm = initialize_llm()
 save_folder = os.getenv("save_folder")
 
 
-def invoke_sample():
-    messages = [
-        ("system", "You are a helpful assistant."),
-        ("human", "{question}"),
-    ]
-
-    prompt = ChatPromptTemplate.from_messages(messages)
-
-    chain = prompt | bedrock_llm | StrOutputParser()
-
-    # Chain Invoke
-    response = chain.invoke({"question": "tell me a joke"})
-    print(response)
-
-def stream_sample():
-    # Stream - Example 1
-    system_message = "You are a helpful assistant that translates {input_language} to {output_language}."
-    human_message = "{text}"
-
-    messages = [
-        ("system", system_message),
-        ("human", human_message)
-    ]
-
-    prompt = ChatPromptTemplate.from_messages(messages)
-
-    chain = prompt | bedrock_llm | StrOutputParser()
-
-    for chunk in chain.stream(
-        {
-            "input_language": "English",
-            "output_language": "Korean",
-            "text": "I love Korean BBQ",
-        }
-    ):
-        print(chunk, end="", flush=True)
-
-    # Stream - Example 2
-    messages = [
-        ("human", "Give me a list of famous tourist attractions in Seoul, Korea")
-    ]
-
-    prompt = ChatPromptTemplate.from_messages(messages)
-
-    chain = prompt | bedrock_llm | StrOutputParser()
-
-    for chunk in chain.stream({}):
-        print(chunk, end="", flush=True)
-
-
-def multimodal_single_image():
-    img1_path = Path(save_folder, "515SFP7ai3L._AC_SL1500_.jpg")
-    img1_base64 = base64.b64encode(img1_path.read_bytes()).decode("utf-8")
-
-    messages = [
-        HumanMessage(
-            content = [
-                {
-                    "type": "image_url",
-                    "image_url": {
-                        "url": f"data:image/png;base64,{img1_base64}",
-                    },
-                },
-                {
-                    "type": "text",
-                    "text": "What's in this image?"
-                },
-            ]
-        )
-    ]
-
-    for chunk in bedrock_llm.stream(messages):
-        print(chunk.content, end="", flush=True)
-
-def multimodal_multiple_images():
-    img1_path = Path(save_folder, "515SFP7ai3L._AC_SL1500_.jpg")
-    img1_base64 = base64.b64encode(img1_path.read_bytes()).decode("utf-8")
-
-    img2_path = Path(save_folder, "61KePAu1JAL._AC_SL1500_.jpg")
-    img2_base64 = base64.b64encode(img2_path.read_bytes()).decode("utf-8")
-
-    messages = [
-        HumanMessage(
-            content = [
-                {
-                    "type": "image_url",
-                    "image_url": {
-                        "url": f"data:image/png;base64,{img1_base64}",
-                    },
-                },
-                {
-                    "type": "image_url",
-                    "image_url": {
-                        "url": f"data:image/png;base64,{img2_base64}",
-                    },
-                },
-                {   
-                    "type": "text",
-                    "text": "Compare two images"
-                },
-            ]
-        )
-    ]
-
-    for chunk in bedrock_llm.stream(messages):
-        print(chunk.content, end="", flush=True)
-
 @tool
 def get_product_info(asin):
     """
@@ -177,23 +70,6 @@ def magic_function(input: int) -> int:
     return input + 2      
 
 search = TavilySearchResults()
-
-def test():
-        prompt = ChatPromptTemplate.from_messages(
-            [
-                ("system", "You are a helpful assistant"),
-                ("placeholder", "{chat_history}"),
-                ("human", "{input}"),
-                ("placeholder", "{agent_scratchpad}"),
-            ]
-        )
-
-        tools = [magic_function]
-
-        agent = create_tool_calling_agent(bedrock_llm, tools, prompt)
-        agent_executor = AgentExecutor(agent=agent, tools=tools, verbose=True)
-
-        agent_executor.invoke({"input": "what is the value of magic_function(3)?"})
 
 def create_listing(asin:str, image_name:str, brand:str, product_features:str):
     print(f"asin: {asin}")
@@ -240,10 +116,4 @@ def create_listing(asin:str, image_name:str, brand:str, product_features:str):
         "description": "description"
     {'}'}'''
     
-    #input = f'''please create a good product listing and output json for amazon asin {asin}'''
-    #print(input)
-    
     return agent_executor.invoke({"input": input})
-
-# create_listing("B0BZYCJK89", "61KePAu1JAL._AC_SL1500_.jpg", "", "")
-# test()
